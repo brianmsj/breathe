24dc73cde0ff5271dcb4a560111c21fc1f35473e
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 13:19:58 -0500

    added backgroundscene container

--- js/components/app.js
+++ js/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import SceneList from '../containers/scene_list';
+import BackgroundScene from '../containers/background_scene';
 
 export default function App() {
   return (
     <div>
       <SceneList />
+      <BackgroundScene />
     </div>
   )
 }
