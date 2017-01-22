a600696a5ce6f2ffa74593b5bb455f1953594672
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 14:03:18 -0500

    added placeholder videos

--- js/containers/background_scene.js
+++ js/containers/background_scene.js
@@ -7,12 +7,48 @@ export class BackgroundScene extends Component {
       return <div>Select a scene to get started.</div>
     }
 
-    return (
-      <div>
-        <h3>Details for:</h3>
-        <div>{this.props.scene.title}</div>
-      </div>
-    )
+    else if(this.props.scene.title === 'Introspection') {
+      return (
+        <div className="app-container">
+          <div className="video-background">
+            <div className="video-foreground">
+              <iframe src="https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w"
+                      frameBorder="0"
+                      allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    else if(this.props.scene.title === 'Halcyon') {
+      return (
+        <div className="app-container">
+          <div className="video-background">
+            <div className="video-foreground">
+              <iframe src="https://www.youtube.com/embed/GyRF2VYrUDs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=GyRF2VYrUDs"
+                      frameBorder="0"
+                      allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    else if(this.props.scene.title === 'Breathe') {
+      return (
+        <div className="app-container">
+          <div className="video-background">
+            <div className="video-foreground">
+              <iframe src="https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U"
+                      frameBorder="0"
+                      allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
   }
 }
 
