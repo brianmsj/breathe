7ef0b10c97dbae3580149ef7e2677751adaa445b
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 15:11:51 -0500

    added url to reducer

--- js/reducers/reducer_scenes.js
+++ js/reducers/reducer_scenes.js
@@ -1,7 +1,16 @@
 export default function() {
   return [
-    {title: 'Introspection'},
-    {title: 'Halcyon'},
-    {title: 'Breathe'}
+    {
+      title: 'Introspection',
+      url: 'https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w'
+    },
+    {
+      title: 'Halcyon',
+      url: 'https://www.youtube.com/embed/GyRF2VYrUDs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=GyRF2VYrUDs'
+    },
+    {
+      title: 'Breathe',
+      url: 'https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U'
+    }
   ]
 }
