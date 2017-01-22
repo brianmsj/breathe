fd755636cdf055a6e184e68975cfb9c00e6e1cae
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 15:05:15 -0500

    replaced hardcoded videos with components

--- js/components/video.js
+++ js/components/video.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function IntrospectionVideo() {
+export default function Video(props) {
   return (
     <div className="app-container">
       <div className="video-background">
         <div className="video-foreground">
-          <iframe src="https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w"
+          <iframe src={props.url}
                   frameBorder="0"
                   allowFullScreen></iframe>
         </div>
--- js/containers/background_scene.js
+++ js/containers/background_scene.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Video from '../components/video';
 
 export class BackgroundScene extends Component {
   render() {
@@ -9,43 +10,19 @@ export class BackgroundScene extends Component {
 
     else if(this.props.scene.title === 'Introspection') {
       return (
-        <div className="app-container">
-          <div className="video-background">
-            <div className="video-foreground">
-              <iframe src="https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w"
-                      frameBorder="0"
-                      allowFullScreen></iframe>
-            </div>
-          </div>
-        </div>
+        <Video url="https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w" />
       )
     }
 
     else if(this.props.scene.title === 'Halcyon') {
       return (
-        <div className="app-container">
-          <div className="video-background">
-            <div className="video-foreground">
-              <iframe src="https://www.youtube.com/embed/GyRF2VYrUDs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=GyRF2VYrUDs"
-                      frameBorder="0"
-                      allowFullScreen></iframe>
-            </div>
-          </div>
-        </div>
+        <Video url="https://www.youtube.com/embed/GyRF2VYrUDs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=GyRF2VYrUDs" />
       )
     }
 
     else if(this.props.scene.title === 'Breathe') {
       return (
-        <div className="app-container">
-          <div className="video-background">
-            <div className="video-foreground">
-              <iframe src="https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U"
-                      frameBorder="0"
-                      allowFullScreen></iframe>
-            </div>
-          </div>
-        </div>
+        <Video url="https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U" />
       )
     }
 
