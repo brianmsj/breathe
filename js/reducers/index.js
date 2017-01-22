abe09d84724b5dec89093fcd9111f0920d099c42
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 13:20:29 -0500

    added activescene reducer to rootreducer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import ScenesReducer from './reducer_scenes';
+import ActiveScene from './reducer_active_scene';
+
 
 const rootReducer = combineReducers({
-  scenes: ScenesReducer
+  scenes: ScenesReducer,
+  activeScene: ActiveScene
 });
 
 export default rootReducer;
