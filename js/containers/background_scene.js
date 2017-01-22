ca8cb2966753a573af13271ce980fc5e7f5fb1f9
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 15:12:08 -0500

    url is now props

--- js/containers/background_scene.js
+++ js/containers/background_scene.js
@@ -10,19 +10,19 @@ export class BackgroundScene extends Component {
 
     else if(this.props.scene.title === 'Introspection') {
       return (
-        <Video url="https://www.youtube.com/embed/W1-wSw2tW_w?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W1-wSw2tW_w" />
+        <Video url={this.props.scene.url} />
       )
     }
 
     else if(this.props.scene.title === 'Halcyon') {
       return (
-        <Video url="https://www.youtube.com/embed/GyRF2VYrUDs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=GyRF2VYrUDs" />
+        <Video url={this.props.scene.url} />
       )
     }
 
     else if(this.props.scene.title === 'Breathe') {
       return (
-        <Video url="https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U" />
+        <Video url={this.props.scene.url} />
       )
     }
 
