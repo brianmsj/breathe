38a69d858e78a30ea870dbe6f6ee9ded8a644c5c
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 11:12:56 -0500

    added image links to state

--- js/reducers/reducer_scenes.js
+++ js/reducers/reducer_scenes.js
@@ -3,16 +3,19 @@ export default function() {
     {
       title: 'Introspection',
       youTubeURL: 'https://www.youtube.com/embed/wPOX3lEpXDU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=wPOX3lEpXDU',
-      soundCloudID: '303750328'
+      soundCloudID: '303750328',
+      image: 'http://i.imgur.com/HWRqyNg.jpg'
     },
     {
       title: 'Halcyon',
       youTubeURL: 'https://www.youtube.com/embed/ZdgM6r1paHY?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=ZdgM6r1paHY',
-      soundCloudID: '304040162'
+      soundCloudID: '304040162',
+      image: 'http://i.imgur.com/HWRqyNg.jpg'
     },
     {
       title: 'Breathe',
-      youTubeURL: 'https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U'
+      youTubeURL: 'https://www.youtube.com/embed/CNYB5Ol489U?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=CNYB5Ol489U',
+      image: 'http://i.imgur.com/HWRqyNg.jpg'
     }
   ]
 }
