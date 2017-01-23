5b16883665e8eb7c9eb5ad72f2cf425f88853631
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 14:42:28 -0500

    added default youtube search

--- js/components/guided_meditation.js
+++ js/components/guided_meditation.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDiIUppadqo2kJs8NVYJpY0fYmZ4CV0EPU';
 
-export default function GuidedMeditation(props) {
-  return (
-    <div>
-      <h1 className="guided-meditation-title">guided meditation component</h1>
-    </div>
-  )
+export default class GuidedMeditation extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'guided meditation'}, (videos) => {
+      this.setState({ videos });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="guided-meditation-title">guided meditation component</h1>
+      </div>
+    )
+  }
 }
