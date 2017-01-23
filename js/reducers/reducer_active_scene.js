299f77725a76e704a3ae16fcfc47f9e2184bc253
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 11:19:35 -0500

    changed image size for testing

--- js/reducers/reducer_active_scene.js
+++ js/reducers/reducer_active_scene.js
@@ -5,7 +5,7 @@ const initialState = {
   image: 'http://i.imgur.com/HWRqyNg.jpg'
 }
 
-export default function(state = initialState, action) {
+export default function(state = null, action) {
   switch(action.type) {
     case 'SCENE_SELECTED':
       return action.payload;
