9fc03c8f8067b78571dea89f53702205f16b0a62
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 12:49:58 -0500

    added fade out to title

--- js/components/scene_title.js
+++ js/components/scene_title.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 
-export default function SceneTitle(props) {
-  return (
-    <h1>{props.title}</h1>
-  )
+export default class SceneTitle extends Component {
+  componentDidMount() {
+    $(function () {
+      var timer;
+      var fadeInBuffer = false;
+      $(document).mousemove(function () {
+        if (!fadeInBuffer) {
+          if (timer) {
+            clearTimeout(timer);
+            timer = 0;
+          }
+
+          $('.scene-title').fadeIn();
+          $('html').css({cursor: ''});
+        }
+
+        else {
+          fadeInBuffer = false;
+        }
+
+        timer = setTimeout(function () {
+          $('.scene-title').fadeOut()
+          $('html').css({cursor: 'none'});
+          fadeInBuffer = true;
+        }, 2000)
+      });
+    });
+  };
+
+  render() {
+    return (
+      <h1 className="scene-title">{this.props.title}</h1>
+    );
+  };
 }
