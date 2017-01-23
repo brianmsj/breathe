5c9ecc55e441413e2193c656ea9160472ed765f1
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 11:29:58 -0500

    updated image link

--- js/reducers/reducer_scenes.js
+++ js/reducers/reducer_scenes.js
@@ -4,7 +4,7 @@ export default function() {
       title: 'Introspection',
       youTubeURL: 'https://www.youtube.com/embed/wPOX3lEpXDU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=wPOX3lEpXDU',
       soundCloudID: '303750328',
-      image: 'http://i.imgur.com/HWRqyNg.jpg'
+      image: 'http://i.imgur.com/HWRqyNg.jpg?1'
     },
     {
       title: 'Halcyon',
