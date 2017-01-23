7a19b442a87655233208a9be0c6049680c88c7bf
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-22 23:58:29 -0500

    added initial state

--- js/reducers/reducer_active_scene.js
+++ js/reducers/reducer_active_scene.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
+const initialState = {
+  title: 'Introspection',
+  youTubeURL: 'https://www.youtube.com/embed/wPOX3lEpXDU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=wPOX3lEpXDU',
+  soundCloudID: '303750328'
+}
+
+export default function(state = initialState, action) {
   switch(action.type) {
     case 'SCENE_SELECTED':
       return action.payload;
