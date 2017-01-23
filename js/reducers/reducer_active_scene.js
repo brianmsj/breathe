4892bb1e54ee544443280b62a84b2ac4442de00a
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 11:13:29 -0500

    added image to initial state

--- js/reducers/reducer_active_scene.js
+++ js/reducers/reducer_active_scene.js
@@ -1,7 +1,8 @@
 const initialState = {
   title: 'Introspection',
   youTubeURL: 'https://www.youtube.com/embed/wPOX3lEpXDU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=wPOX3lEpXDU',
-  soundCloudID: '303750328'
+  soundCloudID: '303750328',
+  image: 'http://i.imgur.com/HWRqyNg.jpg'
 }
 
 export default function(state = initialState, action) {
