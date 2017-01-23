acaa39cd25ff5598b16812ac739588b4eae7c0a1
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 13:53:45 -0500

    initial commit

---
+++ js/components/guided_meditation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const API_KEY = 'AIzaSyDiIUppadqo2kJs8NVYJpY0fYmZ4CV0EPU';
+
+export default function GuidedMeditation(props) {
+  return (
+    <div>
+      <h1>guided meditation component</h1>
+    </div>
+  )
+}
