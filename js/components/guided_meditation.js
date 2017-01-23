987f80149c007318d52b3c32ffac637119b1fe42
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-23 14:18:20 -0500

    styling for header

--- js/components/guided_meditation.js
+++ js/components/guided_meditation.js
@@ -5,7 +5,7 @@ const API_KEY = 'AIzaSyDiIUppadqo2kJs8NVYJpY0fYmZ4CV0EPU';
 export default function GuidedMeditation(props) {
   return (
     <div>
-      <h1>guided meditation component</h1>
+      <h1 className="guided-meditation-title">guided meditation component</h1>
     </div>
   )
 }
