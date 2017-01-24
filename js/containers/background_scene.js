e8ac6756830d4161d4043686d5dda8973757f519
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 16:11:56 -0500

    removed component

--- js/containers/background_scene.js
+++ js/containers/background_scene.js
@@ -3,18 +3,11 @@ import { connect } from 'react-redux';
 import Video from '../components/video';
 import SoundCloud from '../components/soundcloud';
 import SceneTitle from '../components/scene_title';
-import GuidedMeditation from '../components/guided_meditation';
 
 export class BackgroundScene extends Component {
   render() {
     if(!this.props.scene) {
       return <div>Select a scene to get started.</div>
-    }
-
-    else if (this.props.scene.title === 'Guided Meditation') {
-      return (
-        <GuidedMeditation />
-      )
     }
 
     return (
