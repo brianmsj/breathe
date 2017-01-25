1b162a7ac7a4838a7b0761273f49e15c940524e5
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 19:25:04 -0500

    changed scene title

--- js/components/welcome.js
+++ js/components/welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Welcome() {
   return (
     <div>
-      <h1>Welcome to Breathe</h1>
+      <h1 className="welcome">Welcome to Breathe</h1>
       <p>Take a moment to relax and clear your mind</p>
     </div>
   )
