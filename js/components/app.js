bdb04fe4d9eeacf39afb5afd654799810edc39e1
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 19:01:03 -0500

    added welcome component

--- js/components/app.js
+++ js/components/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SceneList from '../containers/scene_list';
 import BackgroundScene from '../containers/background_scene';
+import Welcome from './welcome';
 
 export default function App() {
   return (
     <div className="app-container">
       <SceneList />
       <BackgroundScene />
+      <Welcome />
     </div>
   )
 }
