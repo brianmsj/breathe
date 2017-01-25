49e651d21f65a0afbc1ebc9e55f3044060bb1a16
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 20:05:21 -0500

    changed to class component

--- js/components/welcome.js
+++ js/components/welcome.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Welcome() {
-  return (
-    <div className="welcome-container">
-      <h1 className="welcome">Welcome to <span className="breathe">Breathe</span></h1>
-      <p className="welcome">Take a moment to relax and clear your mind</p>
-    </div>
-  )
+export default class Welcome extends Component {
+  componentDidMount() {
+    $(function () {
+      var timer;
+      var fadeInBuffer = false;
+      $(document).mousemove(function () {
+        if (!fadeInBuffer) {
+          if (timer) {
+            clearTimeout(timer);
+            timer = 0;
+          }
+          $('.welcome-container').fadeIn();
+          $('html').css({cursor: ''});
+        }
+
+        else {
+          fadeInBuffer = false;
+        }
+
+        timer = setTimeout(function () {
+          $('.welcome-container').fadeOut()
+          $('html').css({cursor: 'none'});
+          fadeInBuffer = true;
+        }, 5000)
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="welcome-container">
+        <h1 className="welcome welcome-h1">Welcome to <span className="breathe">Breathe</span></h1>
+        <p className="welcome welcome-p">Take a moment to relax and clear your mind</p>
+      </div>
+    )
+  }
 }
