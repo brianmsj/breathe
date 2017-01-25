ea14eba7b91aea81e65c3a542023b01d3931536b
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-25 17:28:09 -0500

    added mobile message

--- js/components/app.js
+++ js/components/app.js
@@ -2,14 +2,18 @@ import React from 'react';
 import SceneList from '../containers/scene_list';
 import BackgroundScene from '../containers/background_scene';
 import Welcome from './welcome';
+import Mobile from './mobile';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="app-container">
-        <SceneList />
-        <BackgroundScene />
-        <Welcome />
+      <div>
+        <div className="app-container">
+          <SceneList />
+          <BackgroundScene />
+          <Welcome />
+        </div>
+        <Mobile />
       </div>
     )
   }
---
+++ js/components/mobile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Mobile() {
+  return (
+    <div className="mobile">
+      <h1>Please download the Breathe app</h1>
+    </div>
+  )
+}
