5101fd141fa04306607bebdc4797ca1ad20fb742
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 19:35:49 -0500

    added class to div

--- js/components/welcome.js
+++ js/components/welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Welcome() {
   return (
-    <div>
+    <div className="welcome-container">
       <h1 className="welcome">Welcome to <span className="breathe">Breathe</span></h1>
       <p className="welcome">Take a moment to relax and clear your mind</p>
     </div>
