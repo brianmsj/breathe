e07e7198a6d3a8dd3ccb3090bcb3d84f24afa7b4
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 19:27:46 -0500

    styling for welcome message

--- js/components/welcome.js
+++ js/components/welcome.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Welcome() {
   return (
     <div>
-      <h1 className="welcome">Welcome to Breathe</h1>
-      <p>Take a moment to relax and clear your mind</p>
+      <h1 className="welcome">Welcome to <span className="breathe">Breathe</span></h1>
+      <p className="welcome">Take a moment to relax and clear your mind</p>
     </div>
   )
 }
