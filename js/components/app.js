010ceb2d608104f76dd93e1c6e978053ba72e20c
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-24 23:12:09 -0500

    added viewport message placeholder

--- js/components/app.js
+++ js/components/app.js
@@ -2,13 +2,17 @@ import React from 'react';
 import SceneList from '../containers/scene_list';
 import BackgroundScene from '../containers/background_scene';
 import Welcome from './welcome';
+import ViewportMessage from './viewport_message';
 
 export default function App() {
   return (
     <div className="app-container">
-      <SceneList />
-      <BackgroundScene />
-      <Welcome />
+      <div className="main-container">
+        <SceneList />
+        <BackgroundScene />
+        <Welcome />
+      </div>
+      <ViewportMessage />
     </div>
   )
 }
--- js/components/viewport_message.js
+++ js/components/viewport_message.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ViewportMessage() {
   return (
-    <h3>Screen size is too small.</h3>
+    <h3 className="viewport-message">Screen size is too small.</h3>
   )
 }
