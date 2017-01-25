121c832e0c4313c36b8f16fea7e7763e9b843d64
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-25 13:44:44 -0500

    changed react router version

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import BackgroundScene from './containers/background_scene';
+import Video from './components/video';
 
 export default (
-  <Route path='/' component={App} />
+  <Route path='/' component={Video} />
 );
