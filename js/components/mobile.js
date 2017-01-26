09815d14abfe2e41ce1349dc4ddedf5c36ac77df
NOUX-NOVA <peterkim.mh@gmail.com>
2017-01-26 09:57:45 -0500

    edited viewport message

--- js/components/mobile.js
+++ js/components/mobile.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Mobile() {
   return (
     <div className="mobile">
-      <h1>Please download the Breathe app</h1>
+      <h3>Please enjoy Breathe on a wider screen for a fully relaxing experience.</h3>
     </div>
   )
 }
