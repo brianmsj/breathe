97811c35475afc9a2c9afb6de37ee5f518cf9605
NOUX-NOVA <peterkim.mh@gmail.com>
2017-02-21 22:03:39 -0500

    added about

--- js/components/welcome.js
+++ js/components/welcome.js
@@ -25,6 +25,11 @@ export default class Welcome extends Component {
       <div className="welcome-container">
         <h1 className="welcome welcome-h1">Welcome to <span className="breathe">Breathe</span></h1>
         <p className="welcome welcome-p">Take a moment to relax and clear your mind</p>
+        <div className="about">
+          <p className="welcome welcome-p">Breathe is an experimental React application that seamlessly
+            combines API data from YouTube and SoundCloud to help you find
+            peace of mind by presenting relaxing imagery and sounds in full-screen format.</p>
+        </div>
       </div>
     )
   }
